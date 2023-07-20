31483c608a23c192bf59cbf9deb792c996c37f73
5021131 <5021131@it.fcrit.ac.in>
2023-07-20 19:43:18 +0530

    .txt changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const NodeRSA = require('node-rsa')
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
 
 const app = express();
 
@@ -38,33 +39,55 @@ app.post('/encrypt', (req, res) => {
     }
     const image = req.files.image;
     console.log(image);
+    const jsonDataString = JSON.stringify(image);
     mimeType = image.mimetype;
     const data = image.data;
-     fileExtension = path.extname(image.name).toLowerCase();
-    console.log('File extension:', fileExtension);
     console.log(req.body);
     console.log(req.body.publicKey)
     const publicKeyString = req.body.publicKey;   
     const importedpublicKey = new NodeRSA();
     importedpublicKey.importKey(publicKeyString, 'public');
-    encryptedData = importedpublicKey.encrypt(data, 'base64');    
-    res.redirect('/');
+    encryptedData = importedpublicKey.encrypt(jsonDataString, 'base64');    
+    const txtFileName = `${uuidv4()}.txt`;
+  fs.writeFileSync(txtFileName, encryptedData, 'utf-8');
+
+  // Set the response headers for the download
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Disposition', `attachment; filename=${txtFileName}`);
+
+  // Stream the file to the response
+  const fileStream = fs.createReadStream(txtFileName);
+  fileStream.pipe(res);
+
+  // Remove the created text file after download is complete
+  fileStream.on('end', () => {
+    fs.unlinkSync(txtFileName);
+    
+  });
     
 }
 );
 
 app.post('/decrypt', (req, res) => {
-    console.log(req.body);
+  
     const privateKeyString = req.body.privateKey;
-    const encryptedDataString = req.body.ecncryptedData;
-    const importedprivateKey = new NodeRSA();
-    importedprivateKey.importKey(privateKeyString, 'private');
-    const decryptedData = importedprivateKey.decrypt(encryptedDataString, 'binary');
-    res.setHeader('Content-Type', mimeType);
-    res.setHeader('Content-Disposition', `attachment; filename=${uuidv4()}${fileExtension}`);
-    res.send(Buffer.from(decryptedData, 'binary'));
 
+    if (!privateKeyString || !req.files || !req.files.encryptedDataFile) {
+      return res.status(400).json({ error: 'Missing private key or encrypted data file' });
+    }
     
+    const encryptedDataFile = req.files.encryptedDataFile;
+    const encryptedData = encryptedDataFile.data.toString('utf-8');
+   
+    const importedPrivateKey = new NodeRSA(privateKeyString, 'private');
+    
+    const decryptedDataString = importedPrivateKey.decrypt(encryptedData, 'utf-8');
+    const decryptedData = JSON.parse(decryptedDataString);
+    console.log(decryptedData);
+    console.log(decryptedData.mimetype);
+    res.setHeader('Content-Type', decryptedData.mimetype);
+    res.setHeader('Content-Disposition', `attachment; filename=${decryptedData.name}`);
+    res.send(Buffer.from(decryptedData.data, 'binary'));
 
 }
 );
